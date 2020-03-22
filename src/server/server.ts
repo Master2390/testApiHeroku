ad39be97284a08e7612fe596a43adc04781b1ced
Javier Briceño <javierarturob2390@gmail.com>
2020-03-22 19:11:59 -0300

    Carga Inicial

---
+++ src/server/server.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import * as http from 'http'
+import * as bodyParser from 'body-parser'
+import '../config/config';
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ ok: true })
+})
+
+http.createServer(app).listen(process.env.PORT, () => {
+  console.log(`Listener Port: ${process.env.PORT}`);
+});
