88fcf99abc15fec003e462ea1585957414116714
Javier Briceño <javierarturob2390@gmail.com>
2020-05-17 18:06:18 -0400

    algunos punto y coma

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
-import * as http from 'http'
-import * as bodyParser from 'body-parser'
+import * as http from 'http';
+import * as bodyParser from 'body-parser';
 import '../config/config';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.post('/', (req, res) => {
   console.log(req.body);
-  res.status(200).json({ ok: true })
-})
+  res.status(200).json({ ok: true });
+});
 
 http.createServer(app).listen(process.env.PORT, () => {
   console.log(`Listener Port: ${process.env.PORT}`);
